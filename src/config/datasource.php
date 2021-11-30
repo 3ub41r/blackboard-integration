0c1f606cc0dbb0a567c759ef9ae44af22c1e6846
zubair <zubair@utmspace.edu.my>
2021-11-30 11:39:57 +0800

    Enable Foundation

--- src/config/datasource.php
+++ src/config/datasource.php
@@ -2,7 +2,7 @@
 // Add new source classes to this file.
 return [
     // 'App\Classes\ImportDiploma',
-    // 'App\Classes\ImportFoundation',
+    'App\Classes\ImportFoundation',
     // 'App\Classes\ImportIdp',
     'App\Classes\ImportPpsm',
     //  'App\Classes\ImportPpsmDiploma',
